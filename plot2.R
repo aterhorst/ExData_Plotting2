90ea25a07e68b3723b70a6cbb0fa662eace03e99
Andrew Terhorst <Andrew.Terhorst@gmail.com>
2015-11-09 15:25:08 +1100

    question 1, 2

--- plot2.R
+++ plot2.R
@@ -12,15 +12,16 @@ library(reshape)
 NEI <- readRDS("~/ownCloud/Coursera/Exploratory Data Analysis/exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("~/ownCloud/Coursera/Exploratory Data Analysis/exdata_data_NEI_data/Source_Classification_Code.rds")
 
-# Question 1
+# Question 2
 
 sink()
-png("plot1.png", width = 480, height = 480, units = "px")
-tot_emissions <- aggregate(Emissions ~ year, data=NEI, FUN = sum)
+png("plot2.png", width = 480, height = 480, units = "px")
+balt_data <- NEI[which(NEI$fips == '24510'),]
+tot_balt_emissions <- aggregate(Emissions ~ year, data = balt_data, FUN = sum)
 options(scipen = 8)
 par(mar = c(5,8,4,4))
-bp <- barplot(tot_emissions$Emissions, tot_emissions$year, las = 1, names.arg = tot_emissions$year, axes = T, main = "Total PM2.5 Emissions - USA", xlab = "Year")
-title(ylab = "Tons", line = 5)
-lines(bp, tot_emissions$Emissions, lwd = 4, col = "blue")
+bp <- barplot(tot_balt_emissions$Emissions, tot_balt_emissions$year, las = 1, names.arg = tot_balt_emissions$year, axes = T, main = "Total PM2.5 Emissions - Baltimore", xlab = "Year")
+title(ylab = "Tons", line = 4)
+lines(bp, tot_balt_emissions$Emissions, lwd = 4, col = "blue")
 dev.off()
 
